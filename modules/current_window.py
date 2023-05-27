e2f9b266d25d9ee7bae5d9456ffa679e1ba54811
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-27 11:54:28 +0100

    Add docstring to current_window.py

--- modules/current_window.py
+++ modules/current_window.py
@@ -2,6 +2,11 @@ import pygetwindow
 
 class Current_Window:
     def fetch_one() -> dict[str]:
+        """Fetch the current active window on the users PC.
+
+        Returns:
+            dict[str]: Return a dict containing import information.
+        """
         current_active_window: object = pygetwindow.getActiveWindow()
         
         return {"title": current_active_window.title, "width": current_active_window.width, "height": current_active_window.height}
