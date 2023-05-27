dada61c0be4908a3d48411901308cd92cb8131c8
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-27 11:25:46 +0100

    Add docstring

--- modules/tasks.py
+++ modules/tasks.py
@@ -4,8 +4,15 @@ from typing import Union
 
 class Tasks:
     def is_running(executable_name: str) -> Union[str, bool]:
+        """Check if an executable is running by its name.
+
+        Args:
+            executable_name (str): Name of the executable you are looking for.
+
+        Returns:
+            Union[str, bool]: Return str which would be an error else return True or False.
+        """
         found_exodus: bool = False 
-        
         try:
             for process in psutil.process_iter():
                 if process.name() == executable_name and process.status() == "running":
