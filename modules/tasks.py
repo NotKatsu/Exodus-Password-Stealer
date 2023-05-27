3f5b77d14586084713968c44ff6dfc094a658bc2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-27 11:21:55 +0100

    Check if exodus instance is running and if it is return True else return False

--- modules/tasks.py
+++ modules/tasks.py
@@ -1,3 +1,15 @@
 import psutil
 
+from typing import Union
+
 class Tasks:
+    def is_running(executable_name: str) -> Union[str, bool]:
+        try:
+            for process in psutil.process_iter():
+                if process.name() == "Exodus.exe" and process.status() == "running":
+                    return True
+                else:
+                    continue
+                
+        except Exception as error:
+            return error
