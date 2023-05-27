dc6209adbc951b03b206db446b2fef5ae64d4069
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-27 11:48:11 +0100

    fetch the current active window and information about it

--- modules/current_window.py
+++ modules/current_window.py
@@ -1,6 +1,8 @@
 import pygetwindow
 
 class Current_Window:
-    def fetch_one() -> str:
-        return pygetwindow.getActiveWindow()
+    def fetch_one() -> dict[str]:
+        current_active_window: object = pygetwindow.getActiveWindow()
+        
+        return {"title": current_active_window.title, "width": current_active_window.width, "height": current_active_window.height}
         
