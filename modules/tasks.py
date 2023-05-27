c1256780f21fafe10b350896d982e8270a5a1515
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-27 11:23:21 +0100

    Create found_exodus variable and set it

--- modules/tasks.py
+++ modules/tasks.py
@@ -4,10 +4,12 @@ from typing import Union
 
 class Tasks:
     def is_running(executable_name: str) -> Union[str, bool]:
+        found_exodus: bool = False 
+        
         try:
             for process in psutil.process_iter():
                 if process.name() == executable_name and process.status() == "running":
-                    return True
+                    found_exodus: bool = True; return True
                 else:
                     continue
                 
